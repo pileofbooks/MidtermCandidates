7d4a530f913cde502a272653494d467928e5b4d2
Paul Le <ple211@student.cccd.edu>
2020-09-26 22:31:15 -0700

    Project 2 Changes 9-26

--- Project 2/CandidateParsing.py
+++ Project 2/CandidateParsing.py
@@ -3,67 +3,116 @@
 # CS A131
 # Project 2
 
-import csv
-
-
-FILENAME = 'candidates_2018_0921_edited.csv'
-
-
 class Candidate:
     "contains the candidate's profile"
     def __init__(self):
-        fecCandidateID = ""
-        candidateName = ""
-        candidateParty = ""
-        candidateStatus = ""
-        candidateZip = 0
-        fecCommitteeID = ""
-        candidateState = ""
-        candidateDistrict = -1
-        candidateBranch = ""
-        candidateURL = ""
-        candidateCRP = ""
+        self.fecCandidateID = ""
+        self.candidateName = ""
+        self.candidateParty = ""
+        self.candidateStatus = ""
+        self.fecCommitteeID = ""
+        self.candidateState = ""
+        self.candidateDistrict = -1
+        self.candidateBranch = ""
+        self.candidateURL = ""
+        self.candidateCRP = ""
 
     def setfecCandidateID(self, newID: str) -> None:
-        "sets the new FEC candidate ID"
-        fecCandidateID = newID
+        "sets the new FEC(Federal Election Commission) candidate ID"
+        self.fecCandidateID = newID
 
     def setCandidateName(self, newName: str) -> None:
         "sets new candidate name"
-        candidateName = newName
+        self.candidateName = newName
 
     def setCandidateParty(self, newParty) -> None:
         "sets new candidate party"
-        candidateParty = newParty
+        self.candidateParty = newParty
 
     def setCandidateStatus(self, newStatus: str) -> None:
         "sets new candidate status"
-        candidateStatus = newStatus
-
-    def setCandidateZip(self, newZip: int) -> None:
-        "sets new candidate zip code"
-        candidateZip = newZip
+        self.candidateStatus = newStatus
 
     def setfecCommitteeID(self, newID: str) -> None:
-        "sets new FEC committee ID"
-        fecCommitteeID = newID
+        "sets new FEC(Federal Election Commission) committee ID"
+        self.fecCommitteeID = newID
 
     def setCandidateState(self, newState: str) -> None:
         "sets new candidate state"
-        candidateState = newState
+        self.candidateState = newState
 
     def setCandidateDistrict(self, newDistrict: int) -> None:
         "sets new candidate district"
-        candidateDistrict = newDistrict
+        self.candidateDistrict = newDistrict
+
+    def setCandidateBranch(self, newBranch: str) -> None:
+        "sets new candidate branch of Congress"
+        self.candidateBranch = newBranch
 
     def setCandidateURL(self, newURL: str) -> None:
         "sets new candidate website URL"
-        candidateURL = newURL
+        self.candidateURL = newURL
 
     def setCandidateCRP(self, newCRP: str) -> None:
-        "sets new candidate CRP ID"
-        candidateCRP = newCRP
+        "sets new candidate CRP(Center for Responsive Politics) ID"
+        self.candidateCRP = newCRP
 
     def getfecCandidateID(self) -> str:
-        "returns FEC candidate ID"
-        return fecCandidateID
+        "returns FEC(Federal Election Commission) candidate ID"
+        return self.fecCandidateID
+
+    def getCandidateName(self) -> str:
+        "returns candidate name"
+        return self.candidateName
+
+    def getCandidateParty(self) -> str:
+        "returns candidate party"
+        return self.candidateParty
+
+    def getCandidateStatus(self) -> str:
+        "returns candidate status"
+        if self.candidateStatus == 'C':
+            self.candidateStatus = "Challenger"
+        elif self.candidateStatus == 'I':
+            self.candidateStatus = "Incumbent"
+        else:
+            self.candidateStatus = "Open"
+        return self.candidateStatus
+
+    def getfecCommitteeID(self) -> str:
+        "returns candidate's FEC(Federal Election Commission) committee ID"
+        return self.fecCommitteeID
+
+    def getCandidateState(self) -> str:
+        "returns candidate's state"
+        return self.candidateState
+
+    def getCandidateDistrict(self) -> str:
+        "returns candidate's district"
+        return self.candidateDistrict
+
+    def getCandidateBranch(self) -> str:
+        "returns candidate's branch of Congress"
+        return self.candidateBranch
+
+    def getCandidateURL(self) -> str:
+        "returns candidate's website URL"
+        return self.candidateURL
+
+    def getCandidateCRP(self) -> str:
+        "returns candidate's CRP(Center for Responsive Politics) ID"
+        return self.candidateCRP
+
+    def printCandidate(self) -> None:
+        "prints candidate profile"
+        print("Candidate Profile:")
+        print("Name:", self.candidateName)
+        print("Party:", self.candidateParty)
+        print("Location:", self.candidateState + ',', self.candidateDistrict)
+        print("Branch:", self.candidateBranch)
+        print("Status:", self.candidateStatus)
+        print("FEC Candidate ID:", self.fecCandidateID)
+        print("FEC Committee ID:", self.fecCommitteID)
+        print("CRP ID:", self.candidateCRP)
+        print("Candidate URL:")
+        print(self.candidateURL)
