021902748eab86584c9d35323ff81f0bc000d540
pileofbooks <ple211@student.cccd.edu>
2020-09-27 13:44:36 -0700

    9-27 13:43 Changes

--- Project 2/CandidateList.py
+++ Project 2/CandidateList.py
@@ -4,17 +4,26 @@
 # Project 2
 
 
-import Candidate
+import Candidates
 import csv
 
 
 FILENAME = 'candidates_2018_0921_edited.csv'
+stateList = ["AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DC", "DE", "FL", "GA",
+             "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD",
+             "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ",
+             "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC",
+             "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY"]
 
-
-def main():
-    
+def CandidateList(FILENAME: str) -> list:
+    "creates a list of candidates sorted by state"
+    candidateList = [[state] for state in stateList]
     with open(FILENAME, mode = 'r') as csv_file:
         csv_reader = csv.DictReader(FILENAME)
-        for row in range(99):
-            if row == 0:
-                
+        while row < 100:
+            for row in csv_reader(FILENAME):
+                if row == 0:
+                    continue
+                else:
+                    
+    return candidateList
