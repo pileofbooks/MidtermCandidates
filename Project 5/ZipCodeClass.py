a6dab4a4767a8639c171e1ecb76b03e2b4f98559
pileofbooks <ple211@student.cccd.edu>
2020-10-29 18:56:02 -0700

    ZipParser and ZipCodeClass Changes 10-29-2020

--- Project 5/ZipCodeClass.py
+++ Project 5/ZipCodeClass.py
@@ -5,10 +5,12 @@
 
 
 class ZipCode:
-    def __init__(self, newZip: str, newFIPS: int, newDistrict: int):
+    def __init__(self, newZip: str, newFIPS: int, newDistrict: int,
+                 newState: str):
         self.zipCode = newZip
         self.FIPS = newFIPS
         self.district = newDistrict
+        self.state = newState
 
     def getZip(self) -> str:
         "returns self's zip code info"
@@ -21,3 +23,7 @@ class ZipCode:
     def getDistrict(self) -> int:
         "returns self's district number"
         return self.district
+
+    def getState(self) -> str:
+        "returns self's state"
+        return self.state
--- Project 5/zipParser.py
+++ Project 5/zipParser.py
@@ -5,10 +5,23 @@
 
 
 import csv
+from ZipCodeClass import ZipCode
+from python1_data import stateList
 
 
 FILENAME1 = 'State_FIPS_Codes.csv'
 FILENAME2 = 'ZIP_CD_092018.csv'
 
 
-def 
+def FIPSSearcher(searchNum: int) -> str:
+    with open(FILENAME) as csvFile:
+        csvReader = csv.DictReader(csvFile)
+        while csvReader[
+
+def zipList(file1, file2) -> list:
+    with open(FILENAME2) as csvFile:
+        csvReader = csv.DictReader(csvFile)
+        for row in csvReader:
+            zipNumber = row['zip']
+            
+            
