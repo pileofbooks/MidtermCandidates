c132dedfd47bcf45a4ddae6ac98953316da188d9
pileofbooks <ple211@student.cccd.edu>
2020-11-13 18:31:53 -0800

    Old attempts to fix zip code parsing since sunday 11/5/2020

--- Project 5/zipParser.py
+++ Project 5/zipParser.py
@@ -14,20 +14,30 @@ FILENAME2 = 'ZIP_CD_092018.csv'
 
 
 def FIPSSearcher(searchNum: int) -> str:
+    "takes an int and looks through the key to find the state abbreviation"
     state = ''
     with open(FILENAME1) as csvFile:
         csvReader = csv.DictReader(csvFile)
         for row in csvReader:
-            if row['FIPS'] == searchNum:
+            if int(row['FIPS']) == searchNum:
                 state = row['Abbreviation']
     return state
         
 
 def ZipList(dataFile: str) -> list:
+    "sorts zip codes into a 2d list of states using the ZipCode class"
     zipList = [[state] for state in stateList]
     with open(dataFile) as csvFile:
         csvReader = csv.DictReader(csvFile)
         for row in csvReader:
+            cd = row['cd']
+            if cd.isdigit() == False:
+                # some of the cd numbers are not ints, so cannot be processed
+                continue
+            else:
+                cd = int(cd)
+            if (int(cd / 100) > 60):
+                continue
             zipNumber = int(row['zip'])
             if zipNumber < 1000:
                 zipNumber = '00' + str(zipNumber)
@@ -35,12 +45,15 @@ def ZipList(dataFile: str) -> list:
             elif (zipNumber > 1000 and zipNumber < 10000):
                 zipNumber = '0' + str(zipNumber)
                 # print(zipNumber)
-            newZip = ZipCode(zipNumber, int(row['cd'] / 1000),
-                             (row['cd'] % 100),
-                             FIPSSearcher((row['cd'] % 100)))
+            newZip = ZipCode(zipNumber, int(cd / 1000),
+                             (cd % 100),
+                             FIPSSearcher(cd % 100))
+            # print(newZip)
             index = 0
             stateAbbrev = newZip.getState()
-            while (index < len(stateList) and
-                   stateAbbrev != stateList[index]):
-                index += 1
-            
+            # print(stateAbbrev)
+            for state in zipList:
+                # print(state[0])
+                if state[0] == stateAbbrev:
+                    state.append(newZip)
+    return zipList
