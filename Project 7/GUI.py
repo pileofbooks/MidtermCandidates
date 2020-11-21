dc44ea3a66a9767bfa2956b2a4a44343c64bac5d
Paul Le <ple211@student.cccd.edu>
2020-11-21 01:42:27 -0800

    Create GUI.py

---
+++ Project 7/GUI.py
@@ -0,0 +1,25 @@
+# Paul Le
+# Nov 19, 2020
+# CS A131
+# Project 7 - GUI and Help Button
+
+
+import tkinter as tk
+from Candidate import Candidate
+from ZipCodeClass import ZipCode
+import python1_data_manipulator as dataManip
+
+
+# Global Variables
+candidateList = dataManip.midtermCandidates
+stateList = dataManip.statesList
+zipList = dataManip.zipList
+
+# initializing the window
+window = tk.Tk()
+window.geometry('1024x576')
+window.title("Midterm Candidate Search Engine")
+
+def choice(userChoice: int):
+    if userChoice == 1:
+        dataManip.searchByZip(
