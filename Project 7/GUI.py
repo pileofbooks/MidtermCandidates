571a4e8accf1acf02978e93cb635cb43a9e2a158
pileofbooks <ple211@student.cccd.edu>
2020-11-22 22:30:02 -0800

    changes to data manipulator for gui purposes

--- Project 7/GUI.py
+++ Project 7/GUI.py
@@ -7,7 +7,7 @@
 import tkinter as tk
 from Candidate import Candidate
 from ZipCodeClass import ZipCode
-import python1_data_manipulator as dataManip
+import python1_data_manipulator_gui_version as dataManip
 
 
 # Global Variables
@@ -15,13 +15,36 @@ candidateList = dataManip.midtermCandidates
 stateList = dataManip.statesList
 zipList = dataManip.zipList
 
-# initializing the window
-window = tk.Tk()
-window.geometry('1024x576')
-window.title("Midterm Candidate Search Engine")
 
 def choice(userChoice: int):
     if userChoice == 1:
-        dataManip.searchByZip(
+        # user enters input into a text box
+        pass
+
+def help():
+    popup = tk.Tk()
+    popup.geometry('480x600')
+    popup.title("User Guide")
+    label = tk.Label(popup,
+                     text="Below is a help menu for this search engine!")
+    label.font.configure(size=20)
+    lable.grid(column=5, row=1)
 
 
+if __name__ == '__main__':
+    # initializing the window
+    window = tk.Tk()
+    window.geometry('1024x576')
+    window.title("Midterm Candidate Search Engine")
+
+    # labels
+    header = tk.Label(window,
+                      text="Welcome! Please select a filter for candidates!",
+                      )
+    header.grid(column=0, row=1)
+
+    # Buttons
+    button1 = tk.Button(text = "  Zip Code  ", bg="#53a6c2", command=help)
+    button1.grid(column=0, row=4)
+                      
+    window.mainloop()
--- Project 7/python1_data_manipulator_gui_version.py
+++ Project 7/python1_data_manipulator_gui_version.py
@@ -1,7 +1,7 @@
 # Paul Le
 # Sept 27, 2020
 # CS A131
-# Project 2
+# Project 7
 
 
 import python1_data
@@ -9,39 +9,31 @@ from Candidate import Candidate
 from ZipCodeClass import ZipCode
 import ZipParser
 
-
 midtermCandidates = python1_data.CandidateList(python1_data.FILENAME)
 statesList = python1_data.stateList
 zipList = ZipParser.ZipList(ZipParser.FILENAME2)
 
 
-def ZipListSearcher(userInput1: str, userInput2: str) -> list:
+def ZipListSearcher(userInput: str) -> list:
     "nested loop to find the zip code data associated with said code"
     zipCodeMatches = []
-    listIndex = 0
-    while userInput1 != statesList[listIndex]:
-        listIndex += 1
-    # print(zipList[listIndex][0]) (testing for userInput1='CA')
-    for zipCode in zipList[listIndex]:
-        if zipCode == zipList[listIndex][0]:
-            continue
-        temp = str(zipCode.getZip())
-        # print(temp)
-        if temp == userInput2:
-            zipCodeMatches.append(zipCode)
+    for state in zipList:
+        for zipCode in state:
+            if zipCode == state[0]:
+                continue
+            if str(zipCode.getZip()) == userInput:
+                zipCodeMatches.append(zipCode)
     return zipCodeMatches
 
-def searchByZip(dataList: list) -> None: # updated to return list for tkinter
+
+def searchByZip(dataList: list) -> list:
     "locates your local candidate by zip code data"
-    # print("\nHere are your local candidates:")
     result = []
     # nested for loop to find the candidate whose data matches the zip data
     for zipCode in dataList:
         district = zipCode.getDistrict()
         # print(type(district))
         state = zipCode.getState()
-        print('\nFrom district', district, 'in', state,
-              'you have:')
         index = 0
         while state != statesList[index]:
             index += 1
@@ -49,33 +41,35 @@ def searchByZip(dataList: list) -> None: # updated to return list for tkinter
             if candidate == midtermCandidates[index][0]:
                 continue
             if int(candidate.getCandidateDistrict()) == district:
-                print(candidate.getCandidateName())
-                
-def searchByBranch(userInput: str) -> None:
-    "prints all candidates running in said branch of Congress"
-    if userInput == 'H':
-        print('\nHere are all the candidates running for the House',
-              'of Representatives:')
-    elif userInput == 'S':
-        print("\nHere are all the candidates running for the Senate:")
+                result.append(candidate)
+    return result
+
+
+def searchByBranch(userInput: str) -> list:
+    "returns list of all candidates running in said branch of Congress"
+    result = []
     for state in midtermCandidates:
         candidate = 0
         for candidate in state:
             if candidate == state[0]:
                 continue
             if candidate.getCandidateBranch() == userInput:
-                print(candidate.getCandidateName())
+                result.append(candidate)
+    return result
 
-def searchByFECCandidateID(userInput: str) -> None:
+
+def searchByFECCandidateID(userInput: str) -> list:
     "prints the profile of the candidate that matches said FEC Candidate ID"
-    print("\nHere is the candidate that matches this FEC Candidate ID:\n")
+    result = 0
     for state in midtermCandidates:
         candidate = 0
         for candidate in state:
             if candidate == state[0]:
                 continue
             if candidate.getfecCandidateID() == userInput:
-                candidate.printCandidate()
+                result = candidate
+    return result
+
 
 def searchByStatus(userInput: str) -> None:
     "prints all candidates that match said status"
@@ -139,7 +133,7 @@ def main():
         main()
     elif int(userInput) == 5:
         print("All done! Have a good day!")
-        # exit(0)
+        #exit(0)
     else:
         print("Please choose a valid option!")
         main()
